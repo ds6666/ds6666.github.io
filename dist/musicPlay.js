b57a5113c5742ec86d9c6036bf52a42524cce5e6
ds6666 <1047747502@qq.com>
2020-04-04 12:35:14 +0800

    Site updated: 2020-04-04 12:34:44

--- dist/musicPlay.js
+++ dist/musicPlay.js
@@ -17,37 +17,37 @@ const ap = new APlayer({
         {
             name: '你的酒馆对我打了烊',
             artist: '陈雪凝',
-            url: 'http://music.163.com/song/media/outer/url?id=1341964346.mp3',
-            cover: 'http://p1.music.126.net/LiRR__0pJHSivqBHZzbMUw==/109951163816225567.jpg',
+            url: 'https://music.163.com/song/media/outer/url?id=1341964346.mp3',
+            cover: 'https://p1.music.126.net/LiRR__0pJHSivqBHZzbMUw==/109951163816225567.jpg',
             lrc: '/./dist/music/lrc/452986458.lrc',
             theme: '#46718b'
         },
         {
             name: '红昭愿',
             artist: '音阙诗听',
-            url: 'http://music.163.com/song/media/outer/url?id=452986458.mp3',
-            cover: 'http://p1.music.126.net/8ltR3o9R8uJ9_5Cc71cDhA==/109951162951242154.jpg',
+            url: 'https://music.163.com/song/media/outer/url?id=452986458.mp3',
+            cover: 'https://p1.music.126.net/8ltR3o9R8uJ9_5Cc71cDhA==/109951162951242154.jpg',
             theme: '#46718b'
         },
         {
             name: '绿色',
             artist: '陈雪凝',
-            url: 'http://music.163.com/song/media/outer/url?id=1345848098.mp3',
-            cover: 'http://p2.music.126.net/R4ZP3AJ9xV0vvw8LX7AbMA==/109951163860425334.jpg',
+            url: 'https://music.163.com/song/media/outer/url?id=1345848098.mp3',
+            cover: 'https://p2.music.126.net/R4ZP3AJ9xV0vvw8LX7AbMA==/109951163860425334.jpg',
             theme: '#46718b'
         },
 		{
             name: '起风了',
             artist: '买辣椒也用券',
-            url: 'http://music.163.com/song/media/outer/url?id=1330348068.mp3',
-            cover: 'http://p1.music.126.net/diGAyEmpymX8G7JcnElncQ==/109951163699673355.jpg',
+            url: 'https://music.163.com/song/media/outer/url?id=1330348068.mp3',
+            cover: 'https://p1.music.126.net/diGAyEmpymX8G7JcnElncQ==/109951163699673355.jpg',
 	        theme: '#46718b'
         },
         {
             name: '泼墨漓江',
             artist: '泠鸢yousa',
-            url: 'http://music.163.com/song/media/outer/url?id=454905890.mp3',
-            cover: 'http://p2.music.126.net/GULe52JbCtVW0vjXZ1o2yw==/109951162817808719.jpg',
+            url: 'https://music.163.com/song/media/outer/url?id=454905890.mp3',
+            cover: 'https://p2.music.126.net/GULe52JbCtVW0vjXZ1o2yw==/109951162817808719.jpg',
             theme: '#46718b'
         }
     ]
