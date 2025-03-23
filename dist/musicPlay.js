4a7fbceeecefa0d9592492f2d20952254ed25274
ds6666 <1047747502@qq.com>
2025-03-23 22:10:31 +0800

    Site updated: 2025-03-23 22:10:28

--- dist/musicPlay.js
+++ dist/musicPlay.js
@@ -17,7 +17,7 @@ const ap = new APlayer({
         {
             name: '你的酒馆对我打了烊',
             artist: '陈雪凝',
-            url: 'https://music.163.com/song/media/outer/url?id=1341964346.mp3',
+            url: 'https://music-1258548228.cos.ap-beijing.myqcloud.com/music/%E4%BD%A0%E7%9A%84%E9%85%92%E9%A6%86%E5%AF%B9%E6%88%91%E6%89%93%E4%BA%86%E7%83%8A-%E9%99%88%E9%9B%AA%E5%87%9D.mp3',
             cover: 'https://p1.music.126.net/LiRR__0pJHSivqBHZzbMUw==/109951163816225567.jpg',
             lrc: '/./dist/music/lrc/452986458.lrc',
             theme: '#46718b'
@@ -32,14 +32,14 @@ const ap = new APlayer({
         {
             name: '绿色',
             artist: '陈雪凝',
-            url: 'https://music.163.com/song/media/outer/url?id=1345848098.mp3',
+            url: 'https://music-1258548228.cos.ap-beijing.myqcloud.com/music/%E7%BB%BF%E8%89%B2-%E9%99%88%E9%9B%AA%E5%87%9D.mp3',
             cover: 'https://p2.music.126.net/R4ZP3AJ9xV0vvw8LX7AbMA==/109951163860425334.jpg',
             theme: '#46718b'
         },
 		{
             name: '起风了',
             artist: '买辣椒也用券',
-            url: 'https://music.163.com/song/media/outer/url?id=1330348068.mp3',
+            url: 'https://music-1258548228.cos.ap-beijing.myqcloud.com/music/%E8%B5%B7%E9%A3%8E%E4%BA%86-%E4%B9%B0%E8%BE%A3%E6%A4%92%E4%B9%9F%E7%94%A8%E5%88%B8.mp3',
             cover: 'https://p1.music.126.net/diGAyEmpymX8G7JcnElncQ==/109951163699673355.jpg',
 	        theme: '#46718b'
         },
